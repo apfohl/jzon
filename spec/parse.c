33a764fe7515c9e7b26fe3b58f46bfbd2a118915
Andreas Pfohl <mail@apfohl.com>
2016-03-28 19:17:23 +0200

    Added parser tests

---
+++ spec/parse.c
@@ -0,0 +1,48 @@
+#include <speck.h>
+#include <jzon.h>
+
+void spec_parse_empty_object(void)
+{
+    struct jzon *jzon = jzon_parse("{}");
+
+    sp_assert(jzon != NULL);
+    sp_assert(jzon_error.error == JZONE_NONE);
+
+    jzon_free(jzon);
+}
+
+void spec_parse_object(void)
+{
+    struct jzon *jzon =
+        jzon_parse("{\"object\": {\"number\": 42}, \"array\": [1, 2, 3]}");
+
+    sp_assert(jzon != NULL);
+    sp_assert(jzon_error.error == JZONE_NONE);
+
+    sp_assert(
+        jzon_object_get(jzon_object_get(jzon, "object"), "number")->number == 42
+    );
+    sp_assert(jzon_array_get(jzon_object_get(jzon, "array"), 0)->number == 1);
+    sp_assert(jzon_array_get(jzon_object_get(jzon, "array"), 1)->number == 2);
+    sp_assert(jzon_array_get(jzon_object_get(jzon, "array"), 2)->number == 3);
+
+    jzon_free(jzon);
+}
+
+void spec_parse_empty_array(void)
+{
+    struct jzon *jzon = jzon_parse("[]");
+
+    sp_assert(jzon != NULL);
+    sp_assert(jzon_error.error == JZONE_NONE);
+
+    jzon_free(jzon);
+}
+
+void spec_parse_error(void)
+{
+    struct jzon *jzon = jzon_parse("[");
+
+    sp_assert(jzon == NULL);
+    sp_assert(jzon_error.error != JZONE_NONE);
+}
